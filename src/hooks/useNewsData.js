856b83aebad686ffdb5b14ee6d24863c3a2a08c2
kavinprasanth <barryallean7274@gmail.com>
2024-06-19 03:11:49 -0700

    third commit

--- src/hooks/useNewsData.js
+++ src/hooks/useNewsData.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import axios from 'axios';
 
 const useNewsData = (category, searchTerm) => {
   const [newsData, setNewsData] = useState([]);
@@ -12,13 +13,16 @@ const useNewsData = (category, searchTerm) => {
 
         const apiKey = process.env.REACT_APP_GNEWS_API_KEY;
         const searchParam = category ? `q=${category}` : searchTerm ? `q=${searchTerm}` : "q=all";
-        var url = 'https://newsapi.org/v2/everything?' + `${searchParam}&` + `apiKey=${apiKey}`;
-
+        // var url = 'https://newsapi.org/v2/everything?' + `${searchParam}&` + `apiKey=${apiKey}`;
         // const url = `https://newsapi.org/v2/everything?country=IN&category=business&apiKey=${apiKey}`;
         // const url = apiUrl + categoryParam + searchParam;
-        const response = await fetch(url);
-        const data = await response.json();
-        setNewsData(data.articles);
+        // const response = await fetch(url);
+        // const data = await response.json();
+        // setNewsData(data.articles);
+
+        var url = process.env.URI
+        axios.get(url + "/get-news").then((data) => setNewsData(data.articles));
+        
         setLoading(false);
       } catch (error) {
         setError(error);
