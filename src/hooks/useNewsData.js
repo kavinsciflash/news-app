9ebe72142b4a9e26957f245828d6549b9c519b35
kavinprasanth <kavinprasanth7274@gmail.com>
2024-06-20 11:54:54 +0530

    latest commit

--- src/hooks/useNewsData.js
+++ src/hooks/useNewsData.js
@@ -11,26 +11,19 @@ const useNewsData = (category, searchTerm) => {
       try {
         setLoading(true);
 
-        const apiKey = process.env.REACT_APP_GNEWS_API_KEY;
         const searchParam = category ? category : searchTerm ? searchTerm : "all";
-        // var url = 'https://newsapi.org/v2/everything?' + `${searchParam}&` + `apiKey=${apiKey}`;
-        // const url = `https://newsapi.org/v2/everything?country=IN&category=business&apiKey=${apiKey}`;
-        // const url = apiUrl + categoryParam + searchParam;
-        // const response = await fetch(url);
-        // const data = await response.json();
-        // setNewsData(data.articles);
+
+        let uri, body;
 
         if (searchParam) {
-          var url = `https://news-api-cs3h.onrender.com/get-category`;
-          const response = (await axios.post(url, {data: searchParam})).data
-          setNewsData(response.articles);
+          uri = 'http://localhost:5000/get-category';
+          body = { data: searchParam };
         } else {
-          var url = `https://news-api-cs3h.onrender.com/get-news`;
-          const response = (await axios(url)).data
-          setNewsData(response.articles);
+          uri = 'http://localhost:5000/get-news';
         }
 
-
+        const response = await axios.post(uri, body);
+        setNewsData(response.data.articles);
         setLoading(false);
       } catch (error) {
         setError(error);
